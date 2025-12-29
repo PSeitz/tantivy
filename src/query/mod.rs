809c5affe3d933bb836273df3b257f94afd43b85
Pascal Seitz <pascal.seitz@datadoghq.com>
2025-12-29 20:21:54 +0100

    Fix compilation errors after rebase
    - Remove duplicate proptest_boolean_query module
    - Remove duplicate cost() method implementations
    - Fix TopDocs API usage (add .order_by_score())
    - Remove duplicate imports
    - Remove unused variable assignments

--- src/query/mod.rs
+++ src/query/mod.rs
@@ -88,7 +88,7 @@ mod tests {
                 .parse_query(term)
                 .unwrap();
             let top_docs: Vec<(f32, DocAddress)> =
-                searcher.search(&query, &TopDocs::with_limit(10)).unwrap();
+                searcher.search(&query, &TopDocs::with_limit(10).order_by_score()).unwrap();
 
             top_docs.iter().map(|el| el.1.doc_id).collect::<Vec<_>>()
         };
@@ -127,7 +127,7 @@ mod tests {
                 .parse_query(term)
                 .unwrap();
             let top_docs: Vec<(f32, DocAddress)> =
-                searcher.search(&query, &TopDocs::with_limit(10)).unwrap();
+                searcher.search(&query, &TopDocs::with_limit(10).order_by_score()).unwrap();
 
             top_docs.iter().map(|el| el.1.doc_id).collect::<Vec<_>>()
         };
